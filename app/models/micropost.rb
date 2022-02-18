f9dc93e8ee71aeea4e6569c734aa2d502c14b79b
Shivans Agarwal <shivansagarwal@gmail.com>
2022-02-18 11:13:46 +0530

    Finish toy app

---
+++ app/models/micropost.rb
@@ -0,0 +1,5 @@
+class Micropost < ApplicationRecord
+    belongs_to :user
+    validates FILL_IN, presence: true    # Replace FILL_IN with the right code.
+    validates FILL_IN, presence: true    # Replace FILL_IN with the right code.
+end
